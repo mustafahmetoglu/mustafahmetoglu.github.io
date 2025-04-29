82749a25428c640b237669a659f53e7f544a585b
mustafa <61505903+mustafahmetoglu@users.noreply.github.com>
2025-04-29 16:05:22 +0300

    gastronomee

---
+++ precache-manifest.483790e611b8a06865afdcb0b0ac5275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874bd04e67ff49560a3d3fa29efdd670",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4515beb4067c1f376d7",
+    "url": "/static/js/2.4e4eed32.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.4e4eed32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf81e41c3a730d1350b1",
+    "url": "/static/js/main.c2d36635.chunk.js"
+  },
+  {
+    "revision": "9c196e6fccabbf7b0520",
+    "url": "/static/js/runtime-main.eca0a2d2.js"
+  }
+]);
